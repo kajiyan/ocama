5838a3a269626f859d24df1da2c6e6182fa4babc
Hiroki Kajiya <kajiya@draft.jp>
2016-05-02 09:14:45 +0900

    シリアル通信テスト

--- experiments/arduino/arduino-test-0/arduino-test-0.ino
+++ experiments/arduino/arduino-test-0/arduino-test-0.ino
@@ -1,23 +1,47 @@
+String inputString = "";
+boolean stringComplete = false;
+
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
     ;
   }
+
+  inputString.reserve(200);
 //  establishContact();
 }
 
 void loop() {
   Serial.println("TEST");
-  
-  char str[255];
-  
+
+//  serialEvent(); //call the function
+//  // print the string when a newline arrives:
+//  if (stringComplete) {
+//    stringComplete = false;
+//    Serial.println(inputString);
+//    // clear the string:
+//    inputString = "";
+//  }
+
   if (Serial.available()) {
+    char str[255];
     recvStr(str);
     Serial.println(str);
+    Serial.flush();
+    delay(1000);
   }
-  Serial.flush();
   
-  delay(1000);
+  
+//  char str[255];
+//  
+//  if (Serial.available()) {
+//    recvStr(str);
+//    Serial.println(str);
+//  }
+//  Serial.flush();
+//  
+//  delay(1000);
 }
 
 /**
@@ -29,6 +53,24 @@ void establishContact() {
     delay(300);
   }
 }
+
+
+
+/**
+ * serialEvent
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+      Serial.flush();
+    }
+  }
+}
+
+
 
 void recvStr(char *buf) {
   int i = 0;
